a62106878a7ed0a11f49b377f1abe101cc6bdcbd
ishengy <is1048@nyu.edu>
2021-08-22 20:05:09 -0400

    Update run_script.py

--- run_script.py
+++ run_script.py
@@ -28,29 +28,25 @@ btc['Date']  = pd.to_datetime(btc['Date'])
 btc.dropna(subset = ['Adj Close'], inplace=True)
 btc.reset_index(drop=True, inplace=True)
 
-###############################
-#amd = btc
-
+#%%
+df_asset = amd
 n_train = 100
-amd_train = amd.iloc[:n_train]
+df_train = df_asset.iloc[:n_train]
+df_returns = gbm.calc_returns(df_train)
 
-#temp
-amd_returns = gbm.calc_returns(amd_train)
+mu = np.mean(df_returns)
+sigma = np.std(df_returns)
 
-mu = np.mean(amd_returns)
-sigma = np.std(amd_returns)
-
-plt.hist(amd_returns, bins=12, density=True, alpha=0.6)
+plt.hist(df_returns, bins=12, density=True, alpha=0.6)
 xmin, xmax = plt.xlim()
 x_axis = np.linspace(xmin, xmax, 100)
 plt.plot(x_axis, norm.pdf(x_axis, mu, sigma))
 plt.title("Returns Distribution")
 
-sm.qqplot(amd_returns)
+sm.qqplot(df_returns)
 plt.title("Q-Q Plot")
 
-###########################
-
+#%%
 n = 30
 dt = 1
 sim = 10000
@@ -61,10 +57,10 @@ list_nrmse = []
 list_mape = []
 training_size = []
 
-st = np.array(amd['Adj Close'][test_start-1:test_start-1+n].reset_index(drop=True))
+st = np.array(df_asset['Adj Close'][test_start-1:test_start-1+n].reset_index(drop=True))
 
 for i in range(30,110,10):
-    sim_results = gbm.multiple_one_day_GBM(amd, dt, i, n, sim, test_start)
+    sim_results = gbm.multiple_one_day_GBM(df_asset, dt, i, n, sim, test_start)
     acc = gbm.forecasting_acc(st, sim_results)
     list_acc.append(acc)
     list_rmse.append(np.mean(acc['rmse']))
@@ -88,36 +84,35 @@ n = 1
 training_size = []
 p_direction = []
 
-st = np.array(amd['Adj Close'][test_start:test_start+n].reset_index(drop=True))
-s0 = np.array(amd['Adj Close'][test_start-1:test_start-1+n].reset_index(drop=True))
+st = np.array(df_asset['Adj Close'][test_start:test_start+n].reset_index(drop=True))
+s0 = np.array(df_asset['Adj Close'][test_start-1:test_start-1+n].reset_index(drop=True))
 direction = (st-s0) > 0
 
 for i in range(30,110,10):
-    sim_direction = ((gbm.multiple_one_day_GBM(amd, dt, i, n, sim, test_start).T - s0) > 0) == direction
+    sim_direction = ((gbm.multiple_one_day_GBM(df_asset, dt, i, n, sim, test_start).T - s0) > 0) == direction
     p_direction.append(len(sim_direction[sim_direction==True])/sim)
     training_size.append(i)
 
 all_accuracy = pd.DataFrame(list(zip(training_size, list_rmse, list_nrmse,list_mape, p_direction)), 
                                  columns = ['training_size','Expected RMSE','Expected NRMSE','Expected MAPE','P(Correct Direction)'])
 
-######################################
 # 1 sample 30 day path
 n = 30
 dt = 1
 sim = 1
 n_train = 50
 test_start = 150
-sim_results = gbm.multiple_one_day_GBM(amd, dt, n_train, n, sim, test_start)
+sim_results = gbm.multiple_one_day_GBM(df_asset, dt, n_train, n, sim, test_start)
 
-amd_sim = amd[['Date','Adj Close']].iloc[test_start-1:test_start+n-1]
-amd_sim['GBM Sim'] = sim_results
+df_sim = df_asset[['Date','Adj Close']].iloc[test_start-1:test_start+n-1]
+df_sim['GBM Sim'] = sim_results
 
-amd_sim.plot(x='Date')
+df_sim.plot(x='Date')
 plt.title("Normal 30 Business Day Forecast (training set = 50)")
 plt.ylabel("Price")
 plt.xticks(rotation = 45)
 
-###########################
+#%%
 # stationary prolonged
 n = 120
 dt = 1
@@ -129,10 +124,10 @@ list_nrmse = []
 list_mape = []
 training_size = []
 
-st = np.array(amd['Adj Close'][test_start-1:test_start-1+n].reset_index(drop=True))
+st = np.array(df_asset['Adj Close'][test_start-1:test_start-1+n].reset_index(drop=True))
 
 for i in range(30,110,10):
-    sim_results = gbm.multiple_one_day_GBM(amd, dt, i, n, sim, test_start)
+    sim_results = gbm.multiple_one_day_GBM(df_asset, dt, i, n, sim, test_start)
     acc = gbm.forecasting_acc(st, sim_results)
     list_acc.append(acc)
     list_rmse.append(np.mean(acc['rmse']))
@@ -156,19 +151,19 @@ n = 1
 training_size = []
 p_direction = []
 
-st = np.array(amd['Adj Close'][test_start:test_start+n].reset_index(drop=True))
-s0 = np.array(amd['Adj Close'][test_start-1:test_start-1+n].reset_index(drop=True))
+st = np.array(df_asset['Adj Close'][test_start:test_start+n].reset_index(drop=True))
+s0 = np.array(df_asset['Adj Close'][test_start-1:test_start-1+n].reset_index(drop=True))
 direction = (st-s0) > 0
 
 for i in range(30,110,10):
-    sim_direction = ((gbm.multiple_one_day_GBM(amd, dt, i, n, sim, test_start).T - s0) > 0) == direction
+    sim_direction = ((gbm.multiple_one_day_GBM(df_asset, dt, i, n, sim, test_start).T - s0) > 0) == direction
     p_direction.append(len(sim_direction[sim_direction==True])/sim)
     training_size.append(i)
 
 all_accuracy = pd.DataFrame(list(zip(training_size, list_rmse, list_nrmse,list_mape, p_direction)), 
                                  columns = ['training_size','Expected RMSE','Expected NRMSE','Expected MAPE','P(Correct Direction)'])
 
-###########################
+#%%
 #non stationary 
 n = 120
 dt = 1
@@ -181,8 +176,8 @@ training_size = []
 
 for i in range(30,110,10):
     test_start = i+2
-    st = np.array(amd['Adj Close'][test_start-1:test_start-1+n].reset_index(drop=True))
-    sim_results = gbm.moving_GBM(amd, dt, i, n, sim, test_start)
+    st = np.array(df_asset['Adj Close'][test_start-1:test_start-1+n].reset_index(drop=True))
+    sim_results = gbm.moving_GBM(df_asset, dt, i, n, sim, test_start)
     acc = gbm.forecasting_acc(st, sim_results)
     list_acc.append(acc)
     list_rmse.append(np.mean(acc['rmse']))
@@ -208,10 +203,10 @@ p_direction = []
 
 for i in range(30,110,10):
     test_start = i+2
-    st = np.array(amd['Adj Close'][test_start:test_start+n].reset_index(drop=True))
-    s0 = np.array(amd['Adj Close'][test_start-1:test_start-1+n].reset_index(drop=True))
+    st = np.array(df_asset['Adj Close'][test_start:test_start+n].reset_index(drop=True))
+    s0 = np.array(df_asset['Adj Close'][test_start-1:test_start-1+n].reset_index(drop=True))
     direction = (st-s0) > 0
-    sim_direction = ((gbm.moving_GBM(amd, dt, i, n, sim, test_start).T - s0) > 0) == direction
+    sim_direction = ((gbm.moving_GBM(df_asset, dt, i, n, sim, test_start).T - s0) > 0) == direction
     p_direction.append(len(sim_direction[sim_direction==True])/sim)
     training_size.append(i)
 
